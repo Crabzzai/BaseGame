b44508cb2ce325563f63f036ca487f38be7c2345
Bruger <contact@crabzzai.me>
2022-11-23 11:29:33 +0100

    Test Product Added

---
+++ src/ServerStorage/configs/products/TestProduct.ts
@@ -0,0 +1,11 @@
+import { IProductChildObject } from "ServerStorage/types/store/product/IProductChildObject";
+import { DataStoreService, MarketplaceService } from "@rbxts/services";
+
+const Child = {
+	Id: 0,
+	Execute(player, secondPlayer?) {
+		// Product child code here.
+	},
+} as IProductChildObject;
+
+export { Child };
